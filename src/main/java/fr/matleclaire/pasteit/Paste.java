af482a00f21cbd9fa0b60468d1ef9d1c82c456d4
matleclaire <mat.leclaire@gmail.com>
2013-06-16 10:40:26 +0200

    Add some test

---
+++ src/main/java/fr/matleclaire/pasteit/Paste.java
@@ -0,0 +1,60 @@
+package fr.matleclaire.pasteit;
+
+import java.util.Date;
+
+
+public class Paste {
+
+    private String id;
+    private String label;
+    private String text;
+    private Date createdDate;
+
+    public Paste() {
+        this.createdDate = new Date();
+    }
+
+    public Paste(String id, String label, String text) {
+        this.label = label;
+        this.text = text;
+        this.createdDate = new Date();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Paste {" + "id=" + id + ", createdDate ="+ createdDate +",  label=" + label + "}";
+    }
+}
--- src/test/java/fr/matleclaire/pasteit/PasteTest.java
+++ src/test/java/fr/matleclaire/pasteit/PasteTest.java
@@ -1,9 +1,14 @@
 package fr.matleclaire.pasteit;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
+
+import org.junit.Test;
+import static org.fest.assertions.Assertions.assertThat;
 
 public class PasteTest {
 
+    @Test
+    public void test() throws Exception {
+        assertThat(true).isTrue();
+    }
 }

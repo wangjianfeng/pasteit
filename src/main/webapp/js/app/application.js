db84bbe42afc431725d7a5295c631342bca2e228
matleclaire <mat.leclaire@gmail.com>
2013-06-17 22:41:21 +0200

    Add a top bar navigation and a creation form

--- src/main/webapp/js/app/application.js
+++ src/main/webapp/js/app/application.js
@@ -1,3 +1,11 @@
 'use strict';
 
 var pasteApp = angular.module('pasteApp', []);
+
+pasteApp.config(function($routeProvider) {
+    $routeProvider
+        .when('/paste/new', {
+            templateUrl: 'views/paste-new.html',
+            controller : 'newPasteController'
+        });
+});

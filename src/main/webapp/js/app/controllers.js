155a43561f7190b36f71896c3a019174a1bf8fc6
matleclaire <mat.leclaire@gmail.com>
2013-06-17 23:35:04 +0200

    Fix missing scripts and add confirmation after saving

--- src/main/webapp/js/app/controllers.js
+++ src/main/webapp/js/app/controllers.js
@@ -5,6 +5,7 @@ pasteApp.controller("newPasteController" ,function ($scope, $location, Paste) {
     $scope.addPaste = function (paste) {
         Paste.create(paste)
             .success(function (savedPaste) {
+                alert("Your content has been saved!");
                 $scope.paste = {};
             })
             .error(function(resp, statusCode) {
